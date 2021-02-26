5e82cf637b430633a443aac0f891d298afe92774
Jeffrey Kroonen <jeffreykroonen@gmail.com>
2021-02-26 20:35:26 +0100

    Added models

---
+++ app/src/main/java/nl/agilicy/passman/model/Permission.java
@@ -0,0 +1,5 @@
+package nl.agilicy.passman.model;
+
+public class Permission {
+    
+}
---
+++ app/src/main/java/nl/agilicy/passman/model/User.java
@@ -0,0 +1,109 @@
+package nl.agilicy.passman.model;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class User {
+
+    private UUID id;
+
+    private String first_name;
+
+    private String last_name;
+
+    private String email;
+
+    private String password;
+
+    private boolean is_active;
+
+    private boolean is_verified;
+
+    private Timestamp last_login_at;
+
+    private Timestamp created_at;
+
+    private Timestamp updated_at;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(boolean is_active) {
+        this.is_active = is_active;
+    }
+
+    public boolean isIs_verified() {
+        return is_verified;
+    }
+
+    public void setIs_verified(boolean is_verified) {
+        this.is_verified = is_verified;
+    }
+
+    public Timestamp getLast_login_at() {
+        return last_login_at;
+    }
+
+    public void setLast_login_at(Timestamp last_login_at) {
+        this.last_login_at = last_login_at;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    
+}
