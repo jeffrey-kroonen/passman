6c04157987e85ad5eac778f615f6b618f281dbf7
Jeffrey Kroonen <jeffreykroonen@gmail.com>
2021-02-26 19:35:37 +0100

    Initialize project

---
+++ app/src/test/java/nl/agilicy/passman/PassmanApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.agilicy.passman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
