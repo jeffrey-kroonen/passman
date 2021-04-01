43f2e97eefdfb9698bffd20868fc2df56af18e41
Max <maxbogers1998@gmail.com>
2021-04-01 14:13:33 +0200

    show users in table

--- app/src/main/java/nl/agilicy/passman/controller/UserViewController.java
+++ app/src/main/java/nl/agilicy/passman/controller/UserViewController.java
@@ -1,11 +1,39 @@
 package nl.agilicy.passman.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import nl.agilicy.passman.model.Directory;
+import nl.agilicy.passman.service.DirectoryService;
+import nl.agilicy.passman.service.UserService;
+
 @Controller
 public class UserViewController {
+
+    @Autowired
+    private final UserService userService;
+
+    @Autowired
+    private final DirectoryService directoryService;
+
+
+    public UserViewController(UserService userService, DirectoryService directoryService) {
+        this.userService = userService;
+        this.directoryService = directoryService;
+    }
+
+    @GetMapping("/user")
+    public String index(Model model) {
+        model.addAttribute("headTitle", "gebruikers");
+        model.addAttribute("navActive", "manage");
+        model.addAttribute("newDirectory", new Directory());
+        model.addAttribute("directories", this.directoryService.getDirectories());
+        model.addAttribute("users", this.userService.getUsers());
+
+        return "user/index";
+    } 
  
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
